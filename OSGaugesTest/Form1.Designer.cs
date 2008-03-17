ad50658b757ae86933b5f439a66f1503b8b06328
fatmantwo <fatmantwo@681b5970-a948-0410-a3b6-d15f422d4c9f>
2008-03-17 23:20:05 +0000

    pre alpha release

--- OSGaugesTest/Form1.Designer.cs
+++ OSGaugesTest/Form1.Designer.cs
@@ -29,25 +29,16 @@
         private void InitializeComponent()
         {
         	this.components = new System.ComponentModel.Container();
-        	this.button1 = new System.Windows.Forms.Button();
         	this.trackBar1 = new System.Windows.Forms.TrackBar();
         	this.osG_Round1 = new OSGaugesLib.OSG_Round(this.components);
+        	this.osG_Round2 = new OSGaugesLib.OSG_Round(this.components);
+        	this.osG_Round3 = new OSGaugesLib.OSG_Round(this.components);
         	((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
         	this.SuspendLayout();
         	// 
-        	// button1
-        	// 
-        	this.button1.Location = new System.Drawing.Point(410, 28);
-        	this.button1.Name = "button1";
-        	this.button1.Size = new System.Drawing.Size(75, 23);
-        	this.button1.TabIndex = 1;
-        	this.button1.Text = "button1";
-        	this.button1.UseVisualStyleBackColor = true;
-        	this.button1.Click += new System.EventHandler(this.button1_Click);
-        	// 
         	// trackBar1
         	// 
-        	this.trackBar1.Location = new System.Drawing.Point(532, 70);
+        	this.trackBar1.Location = new System.Drawing.Point(32, 334);
         	this.trackBar1.Name = "trackBar1";
         	this.trackBar1.Size = new System.Drawing.Size(236, 42);
         	this.trackBar1.TabIndex = 3;
@@ -58,7 +49,7 @@
         	this.osG_Round1.BackColor = System.Drawing.Color.Transparent;
         	this.osG_Round1.CenterColor = System.Drawing.Color.RoyalBlue;
         	this.osG_Round1.DisplayTickText = true;
-        	this.osG_Round1.Location = new System.Drawing.Point(75, 96);
+        	this.osG_Round1.Location = new System.Drawing.Point(12, 12);
         	this.osG_Round1.Maximum = 100;
         	this.osG_Round1.Minimum = 50;
         	this.osG_Round1.MinimumSize = new System.Drawing.Size(40, 40);
@@ -82,25 +73,86 @@
         	this.osG_Round1.TrimWidth = 6;
         	this.osG_Round1.Value = 50;
         	// 
+        	// osG_Round2
+        	// 
+        	this.osG_Round2.BackColor = System.Drawing.Color.Transparent;
+        	this.osG_Round2.CenterColor = System.Drawing.Color.GhostWhite;
+        	this.osG_Round2.DisplayTickText = true;
+        	this.osG_Round2.Location = new System.Drawing.Point(312, 12);
+        	this.osG_Round2.Maximum = 100;
+        	this.osG_Round2.Minimum = 10;
+        	this.osG_Round2.MinimumSize = new System.Drawing.Size(40, 40);
+        	this.osG_Round2.Name = "osG_Round2";
+        	this.osG_Round2.NeedleColor = System.Drawing.Color.Black;
+        	this.osG_Round2.NeedleLength = 50;
+        	this.osG_Round2.ShowNeedle = true;
+        	this.osG_Round2.Size = new System.Drawing.Size(189, 185);
+        	this.osG_Round2.StrokeNeedle = false;
+        	this.osG_Round2.SurroundColor = System.Drawing.Color.Gainsboro;
+        	this.osG_Round2.TabIndex = 5;
+        	this.osG_Round2.TickAlternateLineLength = 15;
+        	this.osG_Round2.TickFrequency = 13;
+        	this.osG_Round2.TickLineLength = 10;
+        	this.osG_Round2.TickLineOffset = 5;
+        	this.osG_Round2.TicksArcEnd = 450F;
+        	this.osG_Round2.TicksArcStart = 190F;
+        	this.osG_Round2.TicksColor = System.Drawing.Color.Black;
+        	this.osG_Round2.TickTextOffset = 10;
+        	this.osG_Round2.TrimColor = System.Drawing.Color.Black;
+        	this.osG_Round2.TrimWidth = 6;
+        	this.osG_Round2.Value = 0;
+        	// 
+        	// osG_Round3
+        	// 
+        	this.osG_Round3.BackColor = System.Drawing.Color.Transparent;
+        	this.osG_Round3.CenterColor = System.Drawing.Color.LightSteelBlue;
+        	this.osG_Round3.DisplayTickText = true;
+        	this.osG_Round3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.osG_Round3.Location = new System.Drawing.Point(312, 198);
+        	this.osG_Round3.Maximum = 100;
+        	this.osG_Round3.Minimum = 15;
+        	this.osG_Round3.MinimumSize = new System.Drawing.Size(40, 40);
+        	this.osG_Round3.Name = "osG_Round3";
+        	this.osG_Round3.NeedleColor = System.Drawing.Color.Black;
+        	this.osG_Round3.NeedleLength = 0;
+        	this.osG_Round3.ShowNeedle = false;
+        	this.osG_Round3.Size = new System.Drawing.Size(130, 130);
+        	this.osG_Round3.StrokeNeedle = true;
+        	this.osG_Round3.SurroundColor = System.Drawing.Color.SlateGray;
+        	this.osG_Round3.TabIndex = 6;
+        	this.osG_Round3.TickAlternateLineLength = 15;
+        	this.osG_Round3.TickFrequency = 10;
+        	this.osG_Round3.TickLineLength = 10;
+        	this.osG_Round3.TickLineOffset = 5;
+        	this.osG_Round3.TicksArcEnd = 380F;
+        	this.osG_Round3.TicksArcStart = 120F;
+        	this.osG_Round3.TicksColor = System.Drawing.Color.DarkSlateGray;
+        	this.osG_Round3.TickTextOffset = 10;
+        	this.osG_Round3.TrimColor = System.Drawing.Color.DarkGray;
+        	this.osG_Round3.TrimWidth = 6;
+        	this.osG_Round3.Value = 0;
+        	// 
         	// Form1
         	// 
         	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
         	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        	this.ClientSize = new System.Drawing.Size(1142, 529);
+        	this.ClientSize = new System.Drawing.Size(636, 529);
+        	this.Controls.Add(this.osG_Round3);
+        	this.Controls.Add(this.osG_Round2);
         	this.Controls.Add(this.osG_Round1);
         	this.Controls.Add(this.trackBar1);
-        	this.Controls.Add(this.button1);
         	this.Name = "Form1";
-        	this.Text = "Form1";
+        	this.Text = "Speed Gauge Test Form";
         	this.Load += new System.EventHandler(this.Form1_Load);
         	((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
         	this.ResumeLayout(false);
         	this.PerformLayout();
         }
+        private OSGaugesLib.OSG_Round osG_Round3;
+        private OSGaugesLib.OSG_Round osG_Round2;
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TrackBar trackBar1;
         private OSGaugesLib.OSG_Round osG_Round1;
 
